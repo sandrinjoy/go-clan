1fcb207d8eacba55c982d24d4db3e50958e97d17
Sandrin Joy <sandrinejoy007@gmail.com>
2025-07-23 12:44:39 +0200

    refactor main function to set up HTTP server and handle requests

--- clan-castle/main.go
+++ clan-castle/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Welcome to the Clan Castle!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Hello, World!")
+	})
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Printf("Starting server on port %s...\n", port)
+
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
 }
